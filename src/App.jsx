1d13fb4c0a77f7c3aa54a0ded0297601554c3f10
yadavvikas787840@gmail.com <yadavvikas787840@gmail.com>
2025-06-30 15:58:50 +0530

    add home page and navbar complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 import Header from './components/Header/Header'
+import Home from './components/Home/Home'
 
 
 function App() {
   return (
     <>
       <Header />
+      <Home />
     </>
   )
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,22 @@
 import React from 'react'
 import { Link, NavLink } from 'react-router-dom';
 import { RiArrowDropDownLine } from "react-icons/ri";
+import { IoSearch } from "react-icons/io5";
+import { FaBagShopping } from "react-icons/fa6";
+import { FaRegHeart } from "react-icons/fa";
+import { FaRegUserCircle } from "react-icons/fa";
+import { HiBars3CenterLeft } from "react-icons/hi2";
+
+
+
 
 
 function Header() {
     return (
         <>
-            <header className='shadow sticky z-50 top-0'>
+            <header className=' sticky z-50 top-0'>
 
-                <div className="bg-[#262626] text-white px-4 lg:px-6 py-2.5">
+                <div className="bg-[#262626] shadow-2xl border-b-black border-b-2 text-white px-4 lg:px-6 py-2.5">
                     <div className='flex justify-between'>
                         <Link to='/' className='flex items-center '>
                             <img className='mr-3 h-8 ' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTbLiKm04s5vniZdkRwoDzH3A0PKfSoDk855u69d-l0tdLgporrhfZegU-TWxAwsxLyIRc&usqp=CAU" alt="Logo" />
@@ -17,7 +25,7 @@ function Header() {
                         <Link to='#' className='font-semibold text-sm bg-[#7aa93c] hover:bg-[#86ff7b] hover:text-black  px-5 py-2.5 rounded-lg text-white mr-2'>Buy Now</Link>
                     </div>
                 </div>
-                <div className='mt-3'>
+                <div className='p-2 bg-[#22201d] text-white'>
                     <marquee behavior="" direction="left" >Name</marquee>
                 </div>
 
@@ -28,14 +36,17 @@ function Header() {
                             <img className='mr-3 h-12 w-30' src="https://demo2.wpopal.com/olight/wp-content/uploads/2025/04/logo.svg" alt="Logo" />
                         </Link>
 
-                        <div className='flex items-center lg:order-2'>
-                            <Link to='#' className='font-medium text-sm mr-2 py-3 px-5'>Log in</Link>
-                            <Link to='#' className='font-medium text-sm bg-orange-700 hover:bg-orange-800 px-5 py-2.5 rounded-lg text-white mr-2'>Get Started</Link>
+                        <div className='flex items-center gap-2 lg:order-2'>
+                            <NavLink to='/search' className={({ isActive }) => `pr-4 pl-3 py-2 block border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-[#dbaf36] lg:p-0 ${isActive ? "text-[#dbaf36]" : "text-gray-700"}`} ><IoSearch className='text-xl' /></NavLink>
+                            <NavLink to='/shoping' className={({ isActive }) => `pr-4 pl-3 py-2 block border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-[#dbaf36] lg:p-0 ${isActive ? "text-[#dbaf36]" : "text-gray-700"}`} ><FaBagShopping className='text-xl' /></NavLink>
+                            <NavLink to='/like' className={({ isActive }) => `pr-4 pl-3 py-2 block border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-[#dbaf36] lg:p-0 ${isActive ? "text-[#dbaf36]" : "text-gray-700"}`} ><FaRegHeart className='text-xl' /></NavLink>
+                            <NavLink to='/profile' className={({ isActive }) => `pr-4 pl-3 py-2 block border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-[#dbaf36] lg:p-0 ${isActive ? "text-[#dbaf36]" : "text-gray-700"}`} ><FaRegUserCircle className='text-xl' /></NavLink>
+                            <NavLink to='/bars' className={({ isActive }) => `pr-4 pl-3 py-2 block border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-[#dbaf36] lg:p-0 ${isActive ? "text-[#dbaf36]" : "text-gray-700"}`} ><HiBars3CenterLeft className='text-xl' /></NavLink>
                         </div>
                         <div className='hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1' id="mobile-menu-2">
                             <ul className='flex flex-col mt-4 font-medium lg:flex-row lg:gap-8 lg:mt-0' >
                                 <li>
-                                    <NavLink to='/' className={({ isActive }) => `pr-4 pl-3 py-2 block border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0 ${isActive ? "text-orange-700" : "text-gray-700"}`} >
+                                    <NavLink to='/' className={({ isActive }) => `pr-4 pl-3 py-2 block border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-[#dbaf36] lg:p-0 ${isActive ? "text-[#dbaf36]" : "text-gray-700"}`} >
                                         <div className='flex items-center'>
                                             <p>HOME</p>
                                             <p><RiArrowDropDownLine className='text-2xl' /></p>
@@ -43,23 +54,23 @@ function Header() {
                                     </NavLink>
                                 </li>
                                 <li>
-                                    <NavLink to='/shop' className={({ isActive }) => `pr-4 pl-3 py-2 block border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0 ${isActive ? "text-orange-700" : "text-gray-700"}`}>
-                                         <div className='flex items-center'>
+                                    <NavLink to='/shop' className={({ isActive }) => `pr-4 pl-3 py-2 block border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-[#dbaf36] lg:p-0 ${isActive ? "text-[#dbaf36]" : "text-gray-700"}`}>
+                                        <div className='flex items-center'>
                                             <p>SHOP</p>
                                             <p><RiArrowDropDownLine className='text-2xl' /></p>
                                         </div>
                                     </NavLink>
                                 </li>
                                 <li>
-                                    <NavLink to='/categories' className={({ isActive }) => `pr-4 pl-3 py-2 block border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0 ${isActive ? "text-orange-700" : "text-gray-700"}`}>
-                                         <div className='flex items-center'>
+                                    <NavLink to='/categories' className={({ isActive }) => `pr-4 pl-3 py-2 block border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-[#dbaf36] lg:p-0 ${isActive ? "text-[#dbaf36]" : "text-gray-700"}`}>
+                                        <div className='flex items-center'>
                                             <p>CATEGORIES</p>
                                             <p><RiArrowDropDownLine className='text-2xl' /></p>
                                         </div>
                                     </NavLink>
                                 </li>
                                 <li>
-                                    <NavLink to='/product' className={({ isActive }) => `pr-4 pl-3 py-2 block border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0 ${isActive ? "text-orange-700" : "text-gray-700"}`}>
+                                    <NavLink to='/product' className={({ isActive }) => `pr-4 pl-3 py-2 block border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-[#dbaf36] lg:p-0 ${isActive ? "text-[#dbaf36]" : "text-gray-700"}`}>
                                         <div className='flex items-center'>
                                             <p>PRODUCT</p>
                                             <p><RiArrowDropDownLine className='text-2xl' /></p>
@@ -67,7 +78,7 @@ function Header() {
                                     </NavLink>
                                 </li>
                                 <li>
-                                    <NavLink to='/pages' className={({ isActive }) => `pr-4 pl-3 py-2 block border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0 ${isActive ? "text-orange-700" : "text-gray-700"}`}>
+                                    <NavLink to='/pages' className={({ isActive }) => `pr-4 pl-3 py-2 block border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-[#dbaf36] lg:p-0 ${isActive ? "text-[#dbaf36]" : "text-gray-700"}`}>
                                         <div className='flex items-center'>
                                             <p>PAGES</p>
                                             <p><RiArrowDropDownLine className='text-2xl' /></p>
@@ -75,7 +86,7 @@ function Header() {
                                     </NavLink>
                                 </li>
                                 <li>
-                                    <NavLink to='/blog' className={({ isActive }) => `pr-4 pl-3 py-2 block border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0 ${isActive ? "text-orange-700" : "text-gray-700"}`}>
+                                    <NavLink to='/blog' className={({ isActive }) => `pr-4 pl-3 py-2 block border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-[#dbaf36] lg:p-0 ${isActive ? "text-[#dbaf36]" : "text-gray-700"}`}>
                                         <div className='flex items-center'>
                                             <p>BLOG</p>
                                             <p><RiArrowDropDownLine className='text-2xl' /></p>
@@ -83,7 +94,7 @@ function Header() {
                                     </NavLink>
                                 </li>
                                 <li>
-                                    <NavLink to='/contact' className={({ isActive }) => `pr-4 pl-3 py-2 block border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0 ${isActive ? "text-orange-700" : "text-gray-700"}`}>CONTACT</NavLink>
+                                    <NavLink to='/contact' className={({ isActive }) => `pr-4 pl-3 py-2 block border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-[#dbaf36] lg:p-0 ${isActive ? "text-[#dbaf36]" : "text-gray-700"}`}>CONTACT</NavLink>
                                 </li>
                             </ul>
                         </div>
